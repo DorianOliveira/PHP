5c026efddf1352cf53af63e7147e070586784142
DorianTorres-PC\Dorian Torres <dorian_oliveira@yahoo.com.br>
2019-07-04 23:04:53 -0300

    Alteração na classe para receber campos

--- reflection-json.php
+++ reflection-json.php
@@ -1,5 +1,5 @@
 <?php
-function getJson($obj)
+function getJson($obj, $fields=array())
 {
     $reflection = new ReflectionClass($obj);
 
@@ -7,14 +7,54 @@ function getJson($obj)
 
     $array_json = array();
 
-    foreach($props as $prop)
-    {
-        $prop -> setAccessible(true);
 
-        $array_json[$prop -> getName()] = $prop -> getValue($obj);
+    foreach($props as $prop)
+    {   
+    	$valid_property = true;
+
+    	if(is_array($fields) && count($fields) > 0)
+    	{
+    		if(!in_array($prop -> getName(), $fields))
+    			$valid_property = false;
+    	}
+    
+    	if($valid_property)
+    	{
+    		$prop -> setAccessible(true);
+	  		$array_json[$prop -> getName()] = $prop -> getValue($obj);		
+    	}
     }
 
     return json_encode($array_json);
 }
 
+class Teste{
+	private $id;
+	private $name;
+	private $idade;
+
+	public function setId($id)
+	{
+		$this -> id = $id;
+	}
+
+	public function setName($name)
+	{
+		$this -> name = $name;
+	}
+
+	public function setIdade($idade)
+	{
+		$this -> idade = $idade;
+	}
+}
+
+$teste = new Teste();
+
+$teste -> setId('2');
+$teste -> setName('Maria Dolores');
+$teste -> setIdade(38);
+
+echo getJson($teste, ['name', 'id']);
 ?>
+

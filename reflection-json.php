dfacb9a63ac4c4fd6bff87ea8c85e484262c9db6
DorianTorres-PC\Dorian Torres <dorian_oliveira@yahoo.com.br>
2019-07-08 00:46:11 -0300

    - Ajusta da classe para reconhecer objetos

--- reflection-json.php
+++ reflection-json.php
@@ -3,66 +3,91 @@
 namespace JsonHandler;
 
 use ReflectionClass;
-
 class JsonHandler
 {
+    const HEADER = 'Content-Type: application/json';
+    private $jsonData;
 
-	public static function JsonParseObject($obj, $fields=array())
+    public function CreateFromArray($array, $key = '')
+    {
+        $result = $array;
+        
+        if($key != '')
+        {
+            $result = array($key => $array);
+        }
+        $this -> jsonData = json_encode($result);
+    }
+    public  function PrintJson()
+    {
+        header(self :: HEADER);
+        echo $this -> jsonData;
+    } 
+	public function CreateFromObject($obj, $fields=array(), $key = '')
 	{
-	    
 	    $array_json = JsonHandler :: CreateArrayProperties($obj, $fields);
-	    return json_encode( $array_json);
+	    $this -> jsonData = json_encode( $array_json);
 	}
-
-	private static function CreateArrayProperties($obj, $fields = array())
+	private function CreateArrayProperties($obj, $fields = array())
 	{
 		$reflection = new ReflectionClass($obj);
-
 	    $props = $reflection -> getProperties();
-
-	    $array_json = array();
-
-
+		$array_json = array();
+		
+		//var_dump($props);
 	    foreach($props as $prop)
 	    {   
 	    	$valid_property = true;
-
 	    	if(is_array($fields) && count($fields) > 0)
 	    	{
 	    		if(!in_array($prop -> getName(), $fields))
 	    			$valid_property = false;
 	    	}
-	    
+			//echo $valid_property;
 	    	if($valid_property)
 	    	{
-	    		$prop -> setAccessible(true);
-		  		$array_json[$prop -> getName()] = $prop -> getValue($obj);		
+				
+				$prop -> setAccessible(true);
+				
+				//var_dump($prop -> getValue($obj));
+				//echo gettype($prop -> getValue($obj));
+				$type = gettype($prop -> getValue($obj));
+				
+				$value_object = null;
+
+				if($type == 'object')
+				{
+					$value_object = $this -> CreateArrayProperties($prop -> getValue($obj), array());
+				}
+				else
+				{
+					$value_object = $prop -> getValue($obj);
+				}
+				
+				$array_json[$prop -> getName()] = $value_object;
 	    	}
-	    }
-
-	    return $array_json;
-
+		}
+		
+		return $array_json;
 	}
-
-	public static function JsonParseObjectList($listObj, $fields=array())
+	public  function CreateFromListObject($listObj, $fields=array())
 	{
 		$listJson = array();
-
-		$counter_item = 1;
+        $counter_item = 1;
+        
 		foreach($listObj as $obj)
 			$listJson['item' . $counter_item ++] = JsonHandler :: CreateArrayProperties($obj, $fields);
-
-		return json_encode($listJson);
+        
+        $this -> jsonData =  json_encode($listJson);
 	}
-
 }
-
 
 
 class Teste{
 	private $id;
 	private $name;
 	private $idade;
+	private $teste;
 
 	public function setId($id)
 	{
@@ -78,20 +103,33 @@ class Teste{
 	{
 		$this -> idade = $idade;
 	}
+
+	public function getTeste()
+	{
+		return $this -> teste;
+	}
+
+	public function setTeste(Teste $teste)
+	{
+		$this -> teste = $teste;
+	}
 }
 
 $teste = new Teste();
+$teste2 = new Teste();
+$teste3 = new Teste();
 
 $teste -> setId('1');
 $teste -> setName('Maria Dolores');
 $teste -> setIdade(38);
+$teste -> setTeste($teste2);
 
-$teste2 = new Teste();
+
 $teste2 -> setId('2');
 $teste2 -> setName('João Espinafre');
 $teste2 -> setIdade(22);
+$teste3 -> setTeste($teste3);
 
-$teste3 = new Teste();
 $teste3 -> setId('3');
 $teste3 -> setName('Carla Perez');
 $teste3 -> setIdade(36);
@@ -102,13 +140,11 @@ $objs[] = $teste;
 $objs[] = $teste2;
 $objs[] = $teste3;
 
-header('Content-Type: application/json; charset=ISO-8859-1');
+$jsonHandler = new JsonHandler();
 
-//echo JsonHandler :: JsonParseObject($teste, ['name', 'id']);
-
-
-echo JsonHandler :: JsonParseObjectList($objs);
-
+$jsonHandler -> CreateFromObject($teste);
+$jsonHandler -> PrintJson();
+exit;
 ?>
 <script>
 	//Exemplo javascript com jQuery

d5030dee91a65d42cebb06565b30ac2a8869809c
DorianTorres-PC\Dorian Torres <dorian_oliveira@yahoo.com.br>
2019-07-24 21:53:44 -0300

    --

---
+++ config.php
@@ -0,0 +1,21 @@
+
+<!doctype html>
+<html lang="pt-br">
+	<head>
+	  	<meta charset="iso-8859-1">
+
+	  	<title>Testes</title>
+	  	<link href="/projetos/php2/assets/bootstrap/css/bootstrap.css" rel="stylesheet">
+	  	<link href="/projetos/php2/assets/css/styles.css" rel="stylesheet">
+
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.8/styles/default.min.css">
+		<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.8/highlight.min.js"></script>
+
+		<script type="module" src="app.js"></script>
+	</head>
+
+	<body>
+	
+	<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/projetos/php2/components/menu.php'); ?>
+
+	<div class="container-fluid">
